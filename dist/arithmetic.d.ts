3383602ff72a1c5b42866ea9fa7b899db297bbc3
Liran <liranpiade@gmail.com>
2022-09-11 19:38:59 +1000

    Delete dist directory

--- dist/arithmetic.d.ts
+++
@@ -1,9 +0,0 @@
-declare type Length<T extends any[]> = T extends {
-    length: infer L;
-} ? L : never;
-declare type BuildTuple<L extends number, T extends any[] = []> = T extends {
-    length: L;
-} ? T : BuildTuple<L, [...T, any]>;
-export declare type Add<A extends number, B extends number> = Length<[...BuildTuple<A>, ...BuildTuple<B>]>;
-export declare type Subtract<A extends number, B extends number> = BuildTuple<A> extends [...(infer U), ...BuildTuple<B>] ? Length<U> : never;
-export {};
--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { Add, Subtract } from "./arithmetic";
-import { NestedKeyOf, PathOf } from "./nested";
-export { NestedKeyOf, PathOf, Add, Subtract };
-export declare function get<T extends object, P extends NestedKeyOf<T>>(obj: T, path: P): PathOf<T, P>;
